8e0191ad593c0e3b25634c93318b0b431d33f488
Artiom <48484710+PWMaestro@users.noreply.github.com>
2021-05-17 22:39:53 +0300

    Create new class VechileType

--- Autopark/Program.cs
+++ Autopark/Program.cs
@@ -9,4 +9,39 @@ namespace Autopark
             Console.WriteLine("Hello, World!");
         }
     }
+
+    class VechileType
+    {
+        private string typeName;
+        private double taxCoefficient;
+
+        public VechileType(string type, double taxCoefficient = 1.0)
+        {
+            this.typeName = type;
+            this.taxCoefficient = taxCoefficient;
+        }
+
+        public string TypeName
+        {
+            get { return typeName; }
+            set { this.typeName = value; }
+        }
+
+        public double TaxCoefficient
+        {
+            get { return taxCoefficient; }
+            set { this.taxCoefficient = value; }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(
+                "TypeName = {0}\nTaxCoefficient = {1}",
+                this.typeName,
+                this.taxCoefficient
+            );
+        }
+
+        public override string ToString() => $"{typeName}, \"{taxCoefficient}\"";
+    }
 }
