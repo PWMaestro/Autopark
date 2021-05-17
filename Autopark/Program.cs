67bb4911e9ddf388c0ad9588525efabfc0e86fb9
Artiom <48484710+PWMaestro@users.noreply.github.com>
2021-05-17 22:09:58 +0300

    Add project files.

---
+++ Autopark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Autopark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
