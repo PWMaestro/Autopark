37101178d2b89c23bdb016abb02e9b989d052b4f
Artiom <48484710+PWMaestro@users.noreply.github.com>
2021-05-18 10:17:14 +0300

    Replace VechileType class in external file

---
+++ Autopark/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autopark
+{
+    class VechileType
+    {
+        public string TypeName { get; set; }
+        public double TaxCoefficient { get; set; }
+
+        public VechileType(string type, double taxCoefficient = 1.0)
+        {
+            this.TypeName = type;
+            this.TaxCoefficient = taxCoefficient;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(
+                "TypeName = {0}\nTaxCoefficient = {1}",
+                this.TypeName,
+                this.TaxCoefficient
+            );
+        }
+
+        public override string ToString() => $"{TypeName}, \"{TaxCoefficient}\"";
+    }
+}
--- Autopark/Program.cs
+++ Autopark/Program.cs
@@ -37,27 +37,4 @@ namespace Autopark
             Console.WriteLine($"Maximum tax coefficient is {maxTaxCoefficient}.");
         }
     }
-
-    class VechileType
-    {
-        public string TypeName { get; set; }
-        public double TaxCoefficient { get; set; }
-
-        public VechileType(string type, double taxCoefficient = 1.0)
-        {
-            this.TypeName = type;
-            this.TaxCoefficient = taxCoefficient;
-        }
-
-        public void Display()
-        {
-            Console.WriteLine(
-                "TypeName = {0}\nTaxCoefficient = {1}",
-                this.TypeName,
-                this.TaxCoefficient
-            );
-        }
-
-        public override string ToString() => $"{TypeName}, \"{TaxCoefficient}\"";
-    }
 }
