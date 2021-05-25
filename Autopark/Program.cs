1a614474e224c8fbb49379c211b443503c3a271a
Artiom <48484710+PWMaestro@users.noreply.github.com>
2021-05-25 16:10:08 +0300

    Complete task according to level 3

--- Autopark/Program.cs
+++ Autopark/Program.cs
@@ -7,15 +7,15 @@ namespace Autopark
         static void Main(string[] args)
         {
             Printer.PrintArray(Vehicle.vehicles);
-            Array.Sort(Vehicle.vehicles);
             Console.WriteLine();
-            Printer.PrintArray(Vehicle.vehicles);
-            Console.WriteLine("\n");
-
-            Console.WriteLine("Vehicle with minimum maileage is:");
-            Console.WriteLine(Vehicle.GetVehicleWithMinMileage());
-            Console.WriteLine("Vehicle with maximum maileage is:");
-            Console.WriteLine(Vehicle.GetVehicleWithMaxMileage());
+            if (Vehicle.IsSameVehiclesExist())
+            {
+                Printer.PrintArray(Vehicle.GetSameVehicles());
+            }
+            else
+            {
+                Console.WriteLine("There is no same vehicles!");
+            }
         }
     }
 }
