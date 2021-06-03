e44ff8a3c24ab796fb665b887aed1270e4a5bf2b
Artiom <48484710+PWMaestro@users.noreply.github.com>
2021-06-03 20:18:31 +0300

    Add CustomQueue & CustomStack classes

---
+++ Autopark/AbstractCollection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autopark
+{
+    public abstract class AbstractCollection<T> : IEnumerable<T>
+        where T : class
+    {
+        protected T[] _array;
+        protected int _pointer;
+
+        public int Count { get { return _pointer; } }
+
+        protected AbstractCollection()
+        {
+            _array = new T[8];
+            _pointer = 0;
+        }
+
+        protected void Add(T item)
+        {
+            if (_pointer == _array.Length)
+            {
+                var newArray = new T[_array.Length * 2];
+                _array.CopyTo(newArray, 0);
+                _array = newArray;
+            }
+            _array[_pointer++] = item;
+        }
+
+        public void Clear()
+        {
+            _array = new T[8];
+            _pointer = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (var elem in _array)
+            {
+                if (elem.Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _pointer; i++)
+            {
+                yield return _array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < _pointer; i++)
+            {
+                yield return _array[i];
+            }
+        }
+    }
+}
---
+++ Autopark/CustomStack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autopark
+{
+    public class CustomStack<T> : AbstractCollection<T>
+        where T : class
+    {
+        public CustomStack() : base()
+        {
+        }
+
+        public void Push(T item) => Add(item);
+
+        public T Pop()
+        {
+            T lastItem = _array[--_pointer];
+            _array[_pointer] = default;
+            return lastItem;
+        }
+    }
+}
